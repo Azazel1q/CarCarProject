3c6ec68dd826ba606adabb0e5821b4bee6411fe6
Azazel1 <azam.saf21@gmail.com>
2024-02-11 10:44:34 +0500

    v2

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,17 +1,17 @@
 <header>
     <div class="container">
         <div class="header-content">
-            <div class="logo"></div>
+            <div class="logo">CarCar.ru</div>
             <ul class="nav">
-                <li><a class="link" href=""></a></li>
-                <li><a class="link" href=""></a></li>
+                <li><a class="link" href="">Главная</a></li>
+                <li><a class="link" href="">Каталог</a></li>
                 <li><a class="link" href=""></a></li>
                 <li><a class="link" href=""></a></li>
             </ul>
             <div class="flex">
                 @guest
-                    <a href="" class="button">Вход</a>
-                    <a href="" class="button">Регистрация</a>
+                    <a href="{{route('login')}}" class="button">Вход</a>
+                    <a href="{{route('register')}}" class="button">Регистрация</a>
                 @endguest
                 @auth
                     <a href="" class="button">Личный кабинет</a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>@yield('title')</title>
 </head>
 <body>
     <x-header></x-header>
         @yield('content')
-    {{-- <x-footer></x-footer> --}}
     <script src="/js/script.js"></script>
 </body>
 </html>
